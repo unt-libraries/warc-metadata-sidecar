43d6621b7d5b53d6eba1da9b68c33044131301cb
Gracie <gracie.flores@unt.edu>
2021-12-20 13:22:20 -0600

    edit to setup

--- setup.py
+++ setup.py
@@ -15,15 +15,10 @@ setup(
     author='University of North Texas Libraries',
     author_email='gracie.flores@unt.edu',
     license='',
-    py_modules=['warc_metadata_sidecar'],
+    py_modules=['warc-metadata-sidecar'],
     scripts=['warc-metadata-sidecar.py'],
     description='A script that creates a sidecar from a warc file',
     long_description=long_description,
     long_description_content_type='text/markdown',
     install_requires=dependencies,
-    entry_points={
-        'console_scripts': [
-            'warc-metadata-sidecar=warc_metadata_sidecar:main',
-        ]
-    },
 )
