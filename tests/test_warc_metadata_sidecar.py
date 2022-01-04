3b445ecc818da4572e654252059b3efa2d9932b2
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-01-04 08:26:45 -0600

    added tests and test file

--- tests/test_warc_metadata_sidecar.py
+++ tests/test_warc_metadata_sidecar.py
@@ -1,22 +1,54 @@
+import io
 import os
-from logging import ERROR, INFO
-from unittest.mock import Mock, patch, mock_open
+from logging import INFO
 
-import chardet
-import pycld2 as cld2
-from fido.fido import Fido
 from warcio.archiveiterator import ArchiveIterator
 from warcio.recordloader import ArcWarcRecord
-from warcio.warcwriter import WARCWriter
 
-import warc-metadata-sidecar as sidecar
+import warc_metadata_sidecar as sidecar
+
 
 class Test_Warc_Metadata_Sidecar:
-    def test_metadata_sidecar(self):
-        pass
+    def get_record(self):
+        test_file = os.path.join(os.path.dirname(__file__), 'warc.warc')
+        with open(test_file, 'rb') as stream:
+            for record in ArchiveIterator(stream):
+                url = record.rec_headers.get_header('WARC-Target-URI')
+                rawPayload = io.BytesIO(record.raw_stream.read())
+                if record.http_headers:
+                    recordCopy = ArcWarcRecord(
+                        record.format, record.rec_type,
+                        record.rec_headers, rawPayload,
+                        record.http_headers, record.content_type,
+                        record.length
+                        )
+                    decodedPayload = recordCopy.content_stream().read()
+                    rawPayload.seek(0)
+                else:
+                    decodedPayload = rawPayload
+            return(url, rawPayload, decodedPayload)
+
+    def test_metadata_sidecar(self, caplog, tmpdir):
+        print(tmpdir)
+        caplog.set_level(INFO)
+        sidecar.metadata_sidecar(str(tmpdir), 'warc.warc')
+        assert 'Logging WARC metadata record information for warc.warc' in caplog.text
+        assert tmpdir.listdir() == [tmpdir / 'warc.warc.meta']
 
     def test_find_mime_and_puid(self):
-        pass
+        fido = sidecar.ExtendFido()
+        url, rawPayload, decodedPayload = self.get_record()
+        sidecar.find_mime_and_puid(fido, rawPayload, url)
+        assert url == 'https://www.unt.edu'
+        assert fido.puid == 'fmt/471'
+        assert fido.mime == 'text/html'
 
     def test_find_character_set(self):
-        pass
+        url, rawpayload, decodedPayload = self.get_record()
+        result_dict = sidecar.find_character_set(decodedPayload)
+        assert result_dict['encoding'] == 'utf-8'
+
+    def test_find_language(self):
+        url, rawPayload, decodedPayload = self.get_record()
+        language = sidecar.find_language(decodedPayload)
+        assert language['languages']['name'] == 'ENGLISH'
