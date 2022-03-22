ea07fc0780deeb301a85e4cd8d8a750ea935d86d
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-03-22 16:02:41 -0500

    update and add to tests per changes

--- tests/test_warc_metadata_sidecar.py
+++ tests/test_warc_metadata_sidecar.py
@@ -51,10 +51,8 @@ WARCINFO_DICT = {'software': 'warc-metadata-sidecar/1.0',
 
 def test_find_mime_and_puid():
     fido = sidecar.ExtendFido()
-    mime_dict = sidecar.find_mime_and_puid(fido, RECORD1['payload'])
-    assert fido.puid == 'fmt/471'
-    assert fido.mime == 'text/html'
-    assert mime_dict == {'fido': 'text/html', 'python-magic': 'text/html'}
+    mime_and_puid = sidecar.find_mime_and_puid(fido, RECORD1['payload'])
+    assert mime_and_puid == ({'fido': 'text/html', 'python-magic': 'text/html'}, 'fmt/471')
 
 
 def test_find_character_set():
@@ -84,26 +82,32 @@ def test_create_warcinfo_payload():
 
 
 def test_create_string_payload():
-    fido = sidecar.ExtendFido()
-    fido.puid = 'fmt/471'
     mime_dict = {'fido': 'text/html', 'python-magic': 'text/html'}
+    puid = 'fmt/471'
+    mime_and_puid = (mime_dict, puid)
     result_dict = {'encoding': 'ascii', 'confidence': 1.0}
     lang_cld = RECORD_LANG_DICT
-    payload = sidecar.create_string_payload(fido, mime_dict, result_dict, lang_cld)
+    payload = sidecar.create_string_payload(mime_and_puid, result_dict, lang_cld)
     assert payload == '{0} {1}\n{2} {3}\n{4} {5}\n{6} {7}'.format(
                             sidecar.MIME_TITLE, mime_dict,
-                            sidecar.PUID_TITLE, fido.puid,
+                            sidecar.PUID_TITLE, puid,
                             sidecar.CHARSET_TITLE, result_dict,
                             sidecar.LANGUAGE_TITLE, lang_cld)
 
 
 class Test_Warc_Metadata_Sidecar:
 
+    @patch('warc_metadata_sidecar.find_mime_and_puid')
+    @patch('warc_metadata_sidecar.find_character_set')
+    @patch('warc_metadata_sidecar.find_language')
+    @patch('warc_metadata_sidecar.create_string_payload', return_value='payload')
     @patch('warc_metadata_sidecar.create_warcinfo_payload')
     @patch('warc_metadata_sidecar.WARCWriter')
-    def test_metadata_sidecar(self, mock_warcwriter, m_warcinfo, caplog, tmpdir):
+    def test_metadata_sidecar(self, mock_warcwriter, m_warcinfo, m_create_payload, m_lang,
+                              m_charset, m_find_mime, caplog, tmpdir):
         caplog.set_level(INFO)
         writer = mock_warcwriter.return_value
+        m_find_mime.return_value = ({'fido': 'text/html'}, 'fmt/471')
         sidecar.metadata_sidecar(str(tmpdir), TEXT_TEST_FILE)
         assert 'Logging WARC metadata record information for %s', TEXT_TEST_FILE in caplog.text
         assert 'Found 1 record(s)' in caplog.text
@@ -113,6 +117,10 @@ class Test_Warc_Metadata_Sidecar:
         calls = [call(writer.create_warcinfo_record.return_value),
                  call(writer.create_warc_record.return_value)]
         writer.write_record.assert_has_calls(calls)
+        m_create_payload.assert_called_once()
+        m_lang.assert_called_once()
+        m_charset.assert_called_once()
+        m_find_mime.assert_called_once()
 
     @patch('warc_metadata_sidecar.WARCWriter')
     def test_metadata_sidecar_dns_record(self, mock_warcwriter, caplog, tmpdir):
