c8aa42b19439e0859293792709ed92053a44d7da
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-05-09 09:48:06 -0500

    edit docstring, move method call

--- sidecar2cdxj.py
+++ sidecar2cdxj.py
@@ -10,14 +10,14 @@ from warcio.timeutils import iso_date_to_timestamp
 
 
 def create_cdxj_path(sidecar_file, archive_dir):
-    """Creates the path and filename for the CDXJ."""
+    """Take the sidecar file, replace the extension, and return the path/filename of the CDXJ."""
     warc_file = os.path.basename(sidecar_file)
     cdxj_file = re.sub('warc.meta.gz', 'cdxj', warc_file)
     return os.path.join(archive_dir, cdxj_file)
 
 
 def convert_payload_to_dict(record):
-    """Takes a record's payload and puts the fields into a dictionary."""
+    """Parse a record's payload, put the fields into a dictionary and return the dictionary."""
     string_payload = record.content_stream().read().decode('utf-8')
     payload_list = string_payload.split('\n')
     new_dict = {}
@@ -31,8 +31,9 @@ def convert_payload_to_dict(record):
     return new_dict
 
 
-def record_data_to_string(record, new_dict):
-    """Converts record fields and JSON object into a string."""
+def record_data_to_string(record):
+    """Convert dictionary into JSON object, convert record fields and JSON into a string."""
+    new_dict = convert_payload_to_dict(record)
     surt_url = surt.surt(record.rec_headers.get_header('WARC-Target-URI'))
     ts = iso_date_to_timestamp(record.rec_headers.get_header('WARC-Date'))
     return surt_url + ' ' + ts + ' ' + json.dumps(new_dict) + '\n'
@@ -41,8 +42,8 @@ def record_data_to_string(record, new_dict):
 def create_sidecar_cdxj(sidecar_file, archive_dir):
     """Create a CDXJ index from a WARC formatted metadata sidecar file.
 
-    Iterates the metadata records of a WARC metadata sidecar file,
-    and writes a line to a CDXJ file containing the SURT-formatted URI,
+    Iterate the metadata records of a WARC metadata sidecar file,
+    and write a line to a CDXJ file containing the SURT-formatted URI,
     timestamp, and JSON data block representing the record's payload
     key value pairs.
     Keyword arguments:
@@ -58,8 +59,7 @@ def create_sidecar_cdxj(sidecar_file, archive_dir):
         for record in ArchiveIterator(stream):
             if record.rec_type == 'warcinfo':
                 continue
-            new_dict = convert_payload_to_dict(record)
-            record_string = record_data_to_string(record, new_dict)
+            record_string = record_data_to_string(record)
             out.write(record_string)
 
 
