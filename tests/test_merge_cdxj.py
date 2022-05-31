446ab839cbe6216d9786262a1dbb260bfaf7c6e1
gracieflores <53153376+gracieflores@users.noreply.github.com>
2022-05-31 11:34:29 -0500

    add/edit tests per feedback

--- tests/test_merge_cdxj.py
+++ tests/test_merge_cdxj.py
@@ -1,7 +1,7 @@
 import io
 import os
 from logging import INFO
-from unittest.mock import MagicMock, patch, call
+from unittest.mock import patch
 
 import merge_cdxj
 
@@ -60,13 +60,35 @@ def test_not_alpha3_language_code():
 
 
 @patch('merge_cdxj.get_alpha3_language_codes')
-def test_get_sidecar_fields(m_lang):
+def test_get_all_sidecar_fields(m_alpha):
+    m_alpha.return_value = 'eng'
+    lang_list = [{'name': 'ENGLISH', 'code': 'en', 'text-covered': 99, 'score': 1209.0}]
     original_obj = {'mime': 'text/html'}
-    meta_obj = {'Identified-Payload-Type': {'fido': 'image/gif', 'python-magic': 'image/gif'},
+    meta_obj = {'Identified-Payload-Type': {'fido': 'application/xhtml+xml',
+                                            'python-magic': 'text/html'},
+                'Preservation-Identifier': 'fmt/102',
+                'Charset-Detected': {'encoding': 'ascii', 'confidence': 1.0},
+                'Languages-cld2': {'reliable': True, 'text-bytes': 1218,
+                                   'languages': [{'name': 'ENGLISH',
+                                                  'code': 'en',
+                                                  'text-covered': 99,
+                                                  'score': 1209.0}]},
+                'Soft-404-Detected': 0.03782088786303804}
+    actual = merge_cdxj.get_sidecar_fields(original_obj, meta_obj)
+    assert actual['mime-detected'] != 'application/xhtml+xml'
+    assert actual == {'mime': 'text/html', 'mime-detected': 'text/html', 'charset': 'ascii',
+                      'languages': 'eng', 'soft-404-detected': 0.03782088786303804}
+    m_alpha.assert_called_once_with(lang_list)
+
+
+@patch('merge_cdxj.get_alpha3_language_codes')
+def test_get_sidecar_fields(m_alpha):
+    original_obj = {'mime': 'image/gif'}
+    meta_obj = {'Identified-Payload-Type': {'fido': 'image/gif'},
                 'Preservation-Identifier': 'fmt/4'}
     actual = merge_cdxj.get_sidecar_fields(original_obj, meta_obj)
-    assert actual == {'mime': 'text/html', 'mime-detected': 'image/gif'}
-    m_lang.assert_not_called()
+    assert actual == {'mime': 'image/gif', 'mime-detected': 'image/gif'}
+    m_alpha.assert_not_called()
 
 
 @patch('merge_cdxj.get_alpha3_language_codes')
@@ -106,6 +128,28 @@ def test_merge_meta_fields_non_edited(m_fields):
     m_fields.assert_called_once()
 
 
+@patch('merge_cdxj.get_alpha3_language_codes')
+def test_merge_meta_fields_with_duplicate(m_alpha):
+    m_alpha.return_value = 'eng'
+    cdxj = io.StringIO('com,example) 20091111212121 {"url": "http://www.example.com", \
+                                                     "mime": "text/html"}\n'
+                       'com,example) 20091111212121 {"url": "http://www.example.com", '
+                       '"mime": "text/xml"}\n', newline='\n')
+    merged_list = ['com,example) 20091111212121 {"url": "http://www.example.com", '
+                   '"mime": "text/html", "mime-detected": "text/html", '
+                   '"charset": "ascii", "languages": "eng", '
+                   '"soft-404-detected": 0.08195022044249829}\n',
+                   'com,example) 20091111212121 {"url": "http://www.example.com", '
+                   '"mime": "text/xml", "mime-detected": "text/html", '
+                   '"charset": "ascii", "languages": "eng", '
+                   '"soft-404-detected": 0.08195022044249829}\n']
+    original, edited, non_edited = merge_cdxj.merge_meta_fields(META_DICT, cdxj)
+    assert original == merged_list
+    assert not non_edited
+    assert edited == 2
+    m_alpha.assert_called()
+
+
 def test_create_dict_from_meta():
     actual_dict = merge_cdxj.create_dict_from_meta(META_FILE)
     assert actual_dict == META_DICT
@@ -117,31 +161,26 @@ def test_create_cdxj_path(tmpdir):
     assert cdxj_path == expected_path
 
 
-class Test_Merge_Cdxj:
-    @patch('merge_cdxj.create_dict_from_meta')
-    @patch('merge_cdxj.merge_meta_fields')
-    @patch('builtins.open', spec=open)
-    def test_merge_cdxj2(self, mock_open, m_merge_meta, m_create_dict, caplog, tmpdir):
-        caplog.set_level(INFO)
-        merged_file = io.StringIO()
-        m_create_dict.return_value = META_DICT
-        m_merge_meta.return_value = MERGED_LIST, 1, 0
-        handle1 = MagicMock()
-        handle1.__enter__.return_value = merged_file
-        handle1.__exit__.return_value = False
-        handle2 = MagicMock()
-        handle2.__enter__.return_value = META_FILE
-        handle2.__exit__.return_value = False
-        handle3 = MagicMock()
-        handle3.__enter__.return_value = CDXJ
-        handle3.__exit__.return_value = False
-        mock_open.side_effect = (handle1, handle2, handle3)
-        merge_cdxj.merge_cdxjs('meta.cdxj', 'original.cdxj', tmpdir)
-        assert 'Logging CDXJ merge information for original.cdxj and meta.cdxj' in caplog.text
-        assert 'Total edited records: %s', 1 in caplog.text
-        m_create_dict.assert_called_once_with(META_FILE)
-        m_merge_meta.assert_called_once_with(META_DICT, CDXJ)
-        calls = [call(tmpdir/'original_merged.cdxj', 'wt'),
-                 call('meta.cdxj', 'r'),
-                 call('original.cdxj', 'r')]
-        mock_open.assert_has_calls(calls)
+@patch('merge_cdxj.create_dict_from_meta')
+@patch('merge_cdxj.merge_meta_fields')
+def test_merge_cdxj2(m_merge_meta, m_create_dict, caplog, tmpdir):
+    test_dir = os.path.dirname(__file__)
+    meta_file = os.path.join(test_dir, 'meta.cdxj')
+    cdxj_file = os.path.join(test_dir, 'warc_1.cdxj')
+    expected = ['com,example) 20091111212121 {"url": "http://www.example.com", '
+                '"mime": "text/html", "mime-detected": "text/html", "charset": "ascii", '
+                '"languages": "eng", "soft-404-detected": 0.08195022044249829}\n']
+    caplog.set_level(INFO)
+    m_create_dict.return_value = META_DICT
+    m_merge_meta.return_value = MERGED_LIST, 1, 0
+    merge_cdxj.merge_cdxjs(meta_file, cdxj_file, tmpdir)
+    merged_file_path = os.path.join(tmpdir / 'warc_1_merged.cdxj')
+    assert 'Logging CDXJ merge information for {} and {}'.format(cdxj_file,
+                                                                 meta_file) in caplog.text
+    assert 'Total merged records: 1' in caplog.text
+    assert merged_file_path in tmpdir.listdir()
+    m_create_dict.assert_called_once()
+    m_merge_meta.assert_called_once()
+    with open(merged_file_path, 'r') as m_file:
+        lines = m_file.readlines()
+        assert lines == expected
